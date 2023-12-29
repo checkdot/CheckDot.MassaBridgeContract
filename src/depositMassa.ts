2ce5b85895301ae8711d53c837f93c0fd020b252
SunlightLuck <hope4wd88@gmail.com>
2023-12-28 21:59:13 -0500

    update

--- src/depositMassa.ts
+++ src/depositMassa.ts
@@ -11,7 +11,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const CONTRACT_ADDRESS = 'AS1e4vvqMy4RvdSF7xgwMhcu2QhtEfBa8L8QtJSL39AHgGwcCtQn';
+const TOKEN_CONTRACT_ADDRESS =
+  'AS12jyar7DLYxX9fsr1WsTkhMcZEvLSmHFQ1ucqWv2M1wqh7yv7zB';
+const BRIDGE_CONTRACT_ADDRESS =
+  'AS1e4vvqMy4RvdSF7xgwMhcu2QhtEfBa8L8QtJSL39AHgGwcCtQn';
 
 const PRIVATE_KEY = process.env.WALLET_SECRET_KEY;
 
@@ -20,24 +23,41 @@ if (!PRIVATE_KEY) {
 }
 
 (async () => {
-  const account: IAccount = await WalletClient.getAccountFromSecretKey(
-    PRIVATE_KEY,
-  );
+  try {
+    const account: IAccount = await WalletClient.getAccountFromSecretKey(
+      PRIVATE_KEY,
+    );
 
-  const client: Client = await ClientFactory.createDefaultClient(
-    DefaultProviderUrls.BUILDNET,
-    true,
-    account,
-  );
+    const client: Client = await ClientFactory.createDefaultClient(
+      DefaultProviderUrls.BUILDNET,
+      true,
+      account,
+    );
 
-  let res = await client.smartContracts().callSmartContract({
-    maxGas: BigInt(1000000),
-    targetAddress: CONTRACT_ADDRESS,
-    functionName: 'depositMassa',
-    parameter: new Args().addU64(1000000000n).serialize(),
-    fee: MAX_GAS_CALL,
-  });
+    let approveTx = await client.smartContracts().callSmartContract({
+      maxGas: MAX_GAS_CALL,
+      targetAddress: TOKEN_CONTRACT_ADDRESS,
+      functionName: 'approve',
+      parameter: new Args()
+        .addString(BRIDGE_CONTRACT_ADDRESS)
+        .addU64(1000000000n)
+        .serialize(),
+      fee: 0n,
+    });
 
-  console.log(res);
+    console.log(approveTx);
+
+    let depositTX = await client.smartContracts().callSmartContract({
+      maxGas: MAX_GAS_CALL,
+      targetAddress: BRIDGE_CONTRACT_ADDRESS,
+      functionName: 'depositMassa',
+      parameter: new Args().addU64(1000000000n).serialize(),
+      fee: 0n,
+    });
+
+    console.log(depositTX);
+  } catch (err) {
+    console.log(err);
+  }
   process.exit(0); // terminate the process after deployment(s)
 })();
